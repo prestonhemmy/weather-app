07570c264d1c651ca4fb8ed47825f3a22c2b9821
prestonhemmy <phemmy@ufl.edu>
2025-06-02 09:58:10 -0400

    feat: add weather API type definitions

---
+++ src/types/weather.ts
@@ -0,0 +1,93 @@
+export interface GeocodingData {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state?: string;
+  local_names?: {
+    [key: string]: string;
+  }
+}
+
+export interface WeatherData {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: Array<{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+export interface ForecastData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: Array<{
+    dt: number;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+    };
+    weather: Array<{
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }>;
+    clouds: {
+      all: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
+    };
+    visibility: number;
+    pop: number;
+    dt_txt: string;
+  }>;
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
